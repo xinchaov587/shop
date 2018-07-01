4dece7acc5349e2b5d25430bc88353eb018774be
xc <xc521441@gmail.com>
2018-07-01 08:13:19 +0000

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,18 @@ Route::get('/', 'PagesController@root')->name('root');
 // 用户模块脚手架路由
 Auth::routes();
 
+// 用户邮箱验证模块
 Route::group(['middleware' => 'auth'], function() {
+    // 提示用户激活邮箱
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-});
+    // 用户邮箱激活
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    // 用户主动请求发送激活邮件
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     // 开始
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+
     });
     // 结束
 });
